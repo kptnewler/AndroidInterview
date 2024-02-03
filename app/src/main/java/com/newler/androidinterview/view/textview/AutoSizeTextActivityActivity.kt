d45709b3ad732aa585bc129445dde0c6cdc0c062
liule <liule@xd.com>
2024-02-03 11:56:09 +0800

    feat: init commit

---
+++ app/src/main/java/com/newler/androidinterview/view/textview/AutoSizeTextActivityActivity.kt
@@ -0,0 +1,12 @@
+package com.newler.androidinterview.view.textview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.newler.androidinterview.R
+
+class AutoSizeTextActivityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auto_size_text)
+    }
+}
